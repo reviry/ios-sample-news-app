537e0b26bab648054e77655678014162a1dfa138
urakawa <urakawa.0205@gmail.com>
2016-08-28 15:31:31 +0900

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Yuuki Urakawa on 2016/08/28.
+//  Copyright © 2016年 Yuuki Urakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
